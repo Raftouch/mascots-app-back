3107417c0e5ab876dd8152a0cd66640317df801f
raftouch <rafanelkin@gmail.com>
2026-03-05 17:08:40 +0100

    update collaborators response structure

--- controllers/collaborators.js
+++ controllers/collaborators.js
@@ -1,132 +1,90 @@
-const Collaborator = require('../models/collaborator')
-const Mascot = require('../models/mascot')
+const Collaborator = require("../models/collaborator");
+const Mascot = require("../models/mascot");
 
 const getAll = async (req, res) => {
-  let searchOptions = {}
-  if (req.query.name != null && req.query.name !== '') {
-    searchOptions.name = new RegExp(req.query.name, 'i')
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
   }
 
-  let perPage = 5
-  let page = req.query.page || 1
+  let perPage = 5;
+  let page = req.query.page || 1;
 
   try {
     const collaborators = await Collaborator.find(searchOptions)
-      .sort({ name: 'asc' })
+      .sort({ name: "asc" })
       .skip(perPage * page - perPage)
       .limit(perPage)
-      .exec()
-    const count = await Collaborator.count()
+      .exec();
+    const count = await Collaborator.count();
 
-    res.render('collaborators/index', {
+    res.json({
       collaborators: collaborators,
       searchOptions: req.query,
       current: page,
       pages: Math.ceil(count / perPage),
-      layout: '../views/layouts/main',
-    })
+    });
   } catch (error) {
-    res.redirect('/dashboard')
+    res.status(500).json({ error: "Error fetching collaborators" });
   }
-}
-
-const getNew = (req, res) => {
-  res.render('collaborators/new', {
-    collaborator: new Collaborator(),
-    layout: '../views/layouts/main',
-  })
-}
+};
 
 const createOne = async (req, res) => {
-  const collaborator = new Collaborator({ name: req.body.name })
+  const collaborator = new Collaborator({ name: req.body.name });
   try {
-    const newCollaborator = await collaborator.save()
-    res.redirect(`collaborators/${newCollaborator.id}`)
+    const newCollaborator = await collaborator.save();
+    res.status(201).json(newCollaborator);
   } catch (error) {
-    res.render('collaborators/new', {
-      collaborator: collaborator,
-      error_msg: 'Error creating Collaborator',
-      layout: '../views/layouts/main',
-    })
+    res.status(400).json({ error: "Error creating Collaborator" });
   }
-}
+};
 
 const getOne = async (req, res) => {
   try {
-    const collaborator = await Collaborator.findById(req.params.id)
+    const collaborator = await Collaborator.findById(req.params.id);
     const mascots = await Mascot.find({ collaborator: collaborator.id })
       .limit(5)
-      .exec()
-    res.render('collaborators/show', {
+      .exec();
+    res.json({
       collaborator: collaborator,
       mascotsByCollaborator: mascots,
-      layout: '../views/layouts/main',
-    })
+    });
   } catch (error) {
-    res.redirect('/dashboard')
+    res.status(404).json({ error: "Collaborator not found" });
   }
-}
-
-const editOne = async (req, res) => {
-  try {
-    const collaborator = await Collaborator.findById(req.params.id)
-    res.render('collaborators/edit', {
-      collaborator: collaborator,
-      layout: '../views/layouts/main',
-    })
-  } catch (error) {
-    res.redirect('/collaborators')
-  }
-}
+};
 
 const updateOne = async (req, res) => {
-  let collaborator
   try {
-    collaborator = await Collaborator.findById(req.params.id)
-    collaborator.name = req.body.name
-    await collaborator.save()
-    res.redirect(`/collaborators/${collaborator.id}`)
+    const collaborator = await Collaborator.findById(req.params.id);
+    collaborator.name = req.body.name;
+    await collaborator.save();
+    res.json(collaborator);
   } catch (error) {
-    if (collaborator == null) {
-      res.redirect('/dashboard')
-    } else {
-      res.render('collaborators/edit', {
-        collaborator: collaborator,
-        error_msg: 'Error updating Collaborator',
-        layout: '../views/layouts/main',
-      })
-    }
+    res.status(400).json({ error: "Error updating collaborator" });
   }
-}
+};
 
 const deleteOne = async (req, res) => {
-  let collaborator
   try {
-    const collaborator = await Collaborator.findById(req.params.id)
-    const mascots = await Mascot.find({ collaborator: collaborator.id })
-    mascots.length > 0
-      ? res.render('collaborators/show', {
-          collaborator: collaborator,
-          mascotsByCollaborator: mascots,
-          error_msg: 'This collaborator has mascots still',
-          layout: '../views/layouts/main',
-        })
-      : (await collaborator.deleteOne()) && res.redirect('/collaborators')
-  } catch (error) {
-    if (collaborator == null) {
-      res.redirect('/dashboard')
-    } else {
-      res.redirect(`/collaborators/${collaborator.id}`)
+    const collaborator = await Collaborator.findById(req.params.id);
+    const mascots = await Mascot.find({ collaborator: collaborator.id });
+    if (mascots.length > 0) {
+      return res
+        .status(400)
+        .json({ error: "This collaborator has mascots still" });
     }
+    await collaborator.deleteOne();
+    res.json({ success: true });
+  } catch (error) {
+    res.status(400).json({ error: "Error deleting collaborator" });
   }
-}
+};
 
 module.exports = {
   getAll,
-  getNew,
   createOne,
   getOne,
-  editOne,
   updateOne,
   deleteOne,
-}
+};
--- routes/collaborators.js
+++ routes/collaborators.js
@@ -1,28 +1,22 @@
-const express = require('express')
-const router = express.Router()
-const { ensureAuthenticated } = require('../config/auth')
+const express = require("express");
+const router = express.Router();
+const { ensureAuthenticated } = require("../config/auth");
 const {
   getAll,
-  getNew,
   createOne,
   getOne,
-  editOne,
   updateOne,
   deleteOne,
-} = require('../controllers/collaborators')
+} = require("../controllers/collaborators");
 
-router.get('/', ensureAuthenticated, getAll)
+router.get("/", ensureAuthenticated, getAll);
 
-router.get('/new', ensureAuthenticated, getNew)
+router.post("/", ensureAuthenticated, createOne);
 
-router.post('/', ensureAuthenticated, createOne)
+router.get("/:id", ensureAuthenticated, getOne);
 
-router.get('/:id', ensureAuthenticated, getOne)
+router.put("/:id", ensureAuthenticated, updateOne);
 
-router.get('/:id/edit', ensureAuthenticated, editOne)
+router.delete("/:id", ensureAuthenticated, deleteOne);
 
-router.put('/:id', ensureAuthenticated, updateOne)
-
-router.delete('/:id', ensureAuthenticated, deleteOne)
-
-module.exports = router
+module.exports = router;
