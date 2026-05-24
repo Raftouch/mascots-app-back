aaf0a78f8b3da04ee978aa033dcb02fa54d4b145
raftouch <rafanelkin@gmail.com>
2026-05-24 14:54:02 +0200

    fix(models): resolve case-sensitive import issue

--- config/passport.js
+++ config/passport.js
@@ -1,36 +1,36 @@
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcryptjs')
-const User = require('../models/User')
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
       User.findOne({ email: email }).then((user) => {
         if (!user) {
-          return done(null, false, { message: 'That email is not registered' })
+          return done(null, false, { message: "That email is not registered" });
         }
 
         bcrypt.compare(password, user.password, (err, isMatch) => {
-          if (err) throw err
+          if (err) throw err;
           if (isMatch) {
-            return done(null, user)
+            return done(null, user);
           } else {
-            return done(null, false, { message: 'Password incorrect' })
+            return done(null, false, { message: "Password incorrect" });
           }
-        })
-      })
-    })
-  )
+        });
+      });
+    }),
+  );
 
   passport.serializeUser(function (user, cb) {
     process.nextTick(function () {
-      cb(null, { id: user.id, username: user.username })
-    })
-  })
+      cb(null, { id: user.id, username: user.username });
+    });
+  });
 
   passport.deserializeUser(function (user, cb) {
     process.nextTick(function () {
-      return cb(null, user)
-    })
-  })
-}
+      return cb(null, user);
+    });
+  });
+};
