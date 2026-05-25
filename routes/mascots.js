018a300bfa7be5c93627244bb28afe780fb59b31
raftouch <rafanelkin@gmail.com>
2026-05-25 15:03:09 +0200

    Revert "fix(multer): include file extensions in uploaded filenames"
    This reverts commit 75e2c408c91506e8fe096e6a7f3d6985f74cd688.

--- routes/mascots.js
+++ routes/mascots.js
@@ -21,20 +21,8 @@ const {
   deleteOne,
 } = require("../controllers/mascots");
 const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/uploads/mascotImages");
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueName + ext);
-  },
-});
-
 const upload = multer({
-  storage,
+  dest: uploadPath,
   fileFilter: (req, file, callback) => {
     callback(null, imageMimeTypes.includes(file.mimetype));
   },
